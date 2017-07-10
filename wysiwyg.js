af80b000a25187bb5c7800756edd5817bbd324f9
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-10 15:34:52 -0500

    work on setting up JS and creating functions needed to run json files and put them into the DOM

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,4 +1,42 @@
-console.log("greetings.")
+let input = document.getElementById("input");
+let container = document.getElementById("container");
+let card = document.getElementsByClassName("");
+let bio = "";
+let array = [];
+
+// receives XHR information and sets the value to var array
+function setArr() {
+	array = JSON.parse(this.responseText)
+	console.log("array", array)
+	outputCards(array)
+};
+	
+function outputCards(array) {
+	for (i = 0; i < array.length; i++) {
+		
+	}
+};
+
+// function to create the object of the XHR fail
+function XHRFail() {
+	console.log(this.status, this.statusText)
+};
+
+// variable to get XHR to get json files
+var myRequest = new XMLHttpRequest();
+
+//event listeners that will send and get json, or show error if unable 
+myRequest.addEventListener("load", setArr);
+myRequest.addEventListener("error", XHRFail);
+
+// opens json files and sends it to getJson function
+myRequest.open("GET", "items.json");
+	// console.log("data", myRequest);
+myRequest.send();
+
+
+
+
 
 
 
