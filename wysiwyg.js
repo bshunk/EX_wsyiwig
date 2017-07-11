b337f6186fb44c4ac20b79512534a4a6de6ec9ee
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-11 09:31:14 -0500

    get images to link into DOM and display properly

--- wysiwyg.js
+++ wysiwyg.js
@@ -14,7 +14,7 @@ function setArr() {
 function outputCards(peopleArray) {
 	for (i = 0; i < peopleArray.length; i++) {
 		console.log(peopleArray[i].title)
-		container.innerHTML += `<person><header>${peopleArray[i].name} & ${peopleArray[i].title}</header><section>${peopleArray[i].bio} ${peopleArray[i].image}</section><footer>${peopleArray[i].lifespan}</footer></person>`
+		container.innerHTML += `<person><header>${peopleArray[i].name} & ${peopleArray[i].title}</header><section>${peopleArray[i].bio} <img src="${peopleArray[i].image}"></img></section><footer>${peopleArray[i].lifespan}</footer></person>`
 	}
 };
 
@@ -34,9 +34,3 @@ myRequest.addEventListener("error", XHRFail);
 myRequest.open("GET", "items.json");
 	// console.log("data", myRequest);
 myRequest.send();
-
-
-
-
-
-
