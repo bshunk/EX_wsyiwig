3bb32a19cb745eb09dd0e1d7943e99cc4829bc0a
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-12 11:29:42 -0500

    walk through line by line with greg and complete code along / project completely

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,161 +1,147 @@
-// made a variable for the html input element 
+// declared variables to hold objects
 let input = document.getElementById("input");
-
-// made a variable for the entire container that holds all the cards
 let container = document.getElementById("container");
-
-// made a variable for the cards that we'll output into the dom from the output cards func
 let cards = document.getElementsByClassName("cards");
-
-// created an open element bio to put the json into from the XHR
 let bio = "";
-
-// created and empty array to store the entire json file into when called 
 let array = [];
 
-// created the function setArr to receive XHR information and sets the value to var array
+// defines the function setArr 
 function setArr() {
 
-	// set the array to equal the parsed the json file relative to the current functions scope
+	// set array to equal the parsed the json file so it's not displayed like a straight JSON file 
 	array = JSON.parse(this.responseText)
 
-	// executes outputCards function
+	// calls outputCards function and passes array into the function
 	outputCards(array)
 };
 	
-// created outputCards function while have peopleArray in function definition to expect it later
+// defines outputCards function while expecting peopleArray to happen (from line 15, array)
 function outputCards(peopleArray) {
 	
-	// created for loop to iterate through the peopleArray
+	// for loop that iterates through the peopleArray
 	for (var i = 0; i < peopleArray.length; i++) {
 
-		// sets the container to display all the json info through string temperate literals upon getting the json file through the XHR
+		// sets the container to display all the json info through concatenation upon getting the json file through the XHR
 		container.innerHTML += `<div class="cards"><person><header>${peopleArray[i].name} & ${peopleArray[i].title}</header><span class="bio">${peopleArray[i].bio}</span><img src="${peopleArray[i].image}"></img></section><footer>${peopleArray[i].lifespan.birth} ${peopleArray[i].lifespan.death}</footer></person></div>`
 	}
 
-	// executes activateClickEvent function
+	// calls activateClickEvent function
 	activateClickEvent()
 };
 
-// function to create the object of the XHR fail
+// defines the function XHRFail
 function XHRFail() {
 
 	// displays error in the console if the XHR failed to load
 	console.log(this.status, this.statusText)
 };
 
-// variable to get XHR to get json files
+// creates variable myRequest and sets it equal to a new XHR object
 var myRequest = new XMLHttpRequest();
 
-//event listeners that will send and get json
+//adds an event listener so when the page loads, the function setArr is executed
 myRequest.addEventListener("load", setArr);
 
-// event listener that will show error if unable to send and get json
+// adds an event listener so if there is an error, the XHRFail function will run
 myRequest.addEventListener("error", XHRFail);
 
 // opens json files and sends it to getJson function
 myRequest.open("GET", "items.json");
 	
-// console.log("data", myRequest);
+// sends the request 
 myRequest.send();
 
-// created a function that will activate the click event and iterate through the cards
+// defines the function activateClickEvent
 function activateClickEvent() {
 
-	// for loop that iterates through the available cards
+	// for loop that iterates through the cards array
 	for (var i = 0; i < cards.length; i++) {
 
-		// sets each selected card in the iteration to have events / listeners / functions
+		// for each card it adds a "click" event listener and an event function
 		cards[i].addEventListener("click", function (e) {
 
-			// executes function that will clear the input field
+			// calls function clearInputEvent
 			clearInputEvent();
 
-			// executes function that will have the input field react to a click event
+			// calls function activateFocusEvent
 			activateFocusEvent();
 			
-			// executes deathCard function that removes new user input from each card once clicked on another card
+			// calls deathCard function
 			deathCard();
 
-			// executes function that will display a dotted border around the selected card
+			// calls activateBorderEvent function and passes in e.currentTarget
 			activateBorderEvent(e.currentTarget);
 		})
 	}
 };
 
-// function that activates the focus on the input field once clicked by the user
+// defines function activateFocus event
 function activateFocusEvent() {
 
-	// takes the input variable and adds the property focus to react once clicked
+	// takes the input element and adds the property focus to react once clicked
 	input.focus()
 };
 
-// function that runs the border event with the selected card defined/ expected to carry out once function runs
+// defines function activateBorderEvent and expects clickedCard to run (from line 73, e.currentTarget)
 function activateBorderEvent(clickedCard) {
 
-	// adds a class element to the clickedCard object 
-	clickedCard.classList.add("selectedCard")
+	// adds a class element of "selectedCard" to clickedCard
+	clickedCard.classList.add("selectedCard");
 
-	// executes the key event functions while passing clickedCard through to that function
-	activateKeyEvent(clickedCard)
+	// calls activateKeyEvent and passes in clickedCard
+	activateKeyEvent(clickedCard);
 };
 
-// function that removes all information from the card once user clicks another card
+// defines function deathCard
 function deathCard() {
 
-	// for loop that iterates through the selected cards 
+	// for loop that iterates through the cards array
 	for (var i = 0; i < cards.length; i++) {
 
-		// condition stating if the selected card contains a classList of 'selectedCard'
+		// condition checking to see if the selected card contains a classList of "selectedCard"
 		if (cards[i].classList.contains("selectedCard")) {
 
-			// removes the data from the condition if it met in line above
+			// if true, removes the data from the condition
 			cards[i].classList.remove("selectedCard")
 		}
 	}
 };
 
-// function that creates a click event when user types / keysup in the input field
+// defines function activateKeyEvent and passes in clickedCard (from line 92, clickedCard)
 function activateKeyEvent(clickedCard) {
 
-	// added the keyup event listener to the input field and created an event function
+	// adds an event listener to the keyup in the input field and runs an event function
 	input.addEventListener("keyup", function(e) {
 
-		// console log to see if event worked
-		console.log("event", e);
-
-		// condition that sees if the key stroke was a return/enter from the user
+		// condition that checks to see if the key stroke was a return/enter from the user
 		if (e.keyCode === 13) {
 
-			// executes function that clers the input field
+			// if true, calls clearInputEvent
 			clearInputEvent()
 
-		// else condition that will run if the above if statement was false 
+		// otherwise if false
 		}else {
 
-		// instead of clearing input, the condition will output new data in the bio DOM
+		// calls mirrorText function and passes in clickedCard
 		mirrorText(clickedCard);
 		}
 	});
 };
 
-// created a function that will output exactly what the user inputs into the DOM for a card's bio section
+// defines function mirrorText and expects clickedCard to run (from line 126, clickedCard)
 function mirrorText(clickedCard) {
-	
-	//console log that tests whether the input.value ran properly 
-	console.log("input value", input.value);
 
-	// if statement that sets the condition if a clickedCard class contains the selectedCard class
+	// condition that checks to see if the selected card contains a class of "selectedCard"
 	if (clickedCard.classList.contains("selectedCard")) {
 
-		// it if statement is true, sets the current bio to the user input data and replaces original json information
+		// if true, the area within the bio class that's inside of the span gets equaled to the value of the user input field
 		clickedCard.querySelector(".bio").innerHTML = input.value;
 	}
 };
 
-// function that runs the clear input field
+// defines function clearInputEvent
 function clearInputEvent() {
 
-	// sets the input field to nothing/ aka removes any data in the field
+	// sets the input field to the value of an empty string
 	input.value = "";
 };
